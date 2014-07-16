fc209ed8aafa7602e8b4d2c8fa235fae3f6b3bc3
bsplu <ruc.bs.plu@hotmail.com>
2014-07-16 21:30:54 +0800

    readfile.cpp可以列出文件夹下所有文件列表

--- c4r.cpp
+++ c4r.cpp
@@ -4,39 +4,25 @@
  */
 #include<iostream>
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<fstream>
+#include<iomanip>
+#include<io.h>
+#include<time.h>
+#include <sstream>
+
 
 using namespace std;
+
+int sys(int s);
 
 int main(){
 
 
+	sys(1);
 
-	system("touch test.txt");
 
 	return 0;
-}
-
-/*save函数
- * 用来记录保存内容
- * 每调用一次save函数就把当下需要同步的文件copy一份出来
- *
- *
- */
-int save(void){
-
-	//将要保存的文件名从save.list中读出{
-
-	//}================================
-
-
-	//检查文件是否存在{
-
-	//存在}============================
-
-	//存在，将文件打包
-
-	//添加到存档文件中
 }
 
 
@@ -47,16 +33,43 @@ int save(void){
  */
 int sys(int s){
 
-	string path;//要同步的目录名字
+
+
+	string ssavepath;//要同步的目录名字
+
+	string sc4rbpath;//c4r储存的目录
+	string starfilename;
+	stringstream stime;
+
+	ssavepath = ".";
+
+	sc4rbpath = "D:/workspace";
+
+
+	stime << int(time(NULL));
+	starfilename = (stime.str() + ".tar.gz").c_str();
+
+	//检查sc4r目录是否存在
+	if(_access((sc4rbpath+"/.c4rb").c_str(),0) == -1){
+		system(("powershell \"mkdir " + sc4rbpath + "/.c4rb \"").c_str());
+	}
+
 	//如果要同步所有文件，则不会罗列出还没有同步的文件列表
 	if(s == 1){
 		//表示全部同步
-		//刷新整个列表
-		AddSaveList(pfile,path);]
 
+		ofstream fout((sc4rbpath + "/.c4rb/save.list").c_str());
+
+		//刷新整个列表
 		//重新创建save.list
+		//AddSaveList(fout,ssavepath);
 
 		//将整个目录打包
+		system(("tar --exclude " + sc4rbpath + "/.c4rb -zcvf "+sc4rbpath+"/.c4rb/" + starfilename + " " +
+				ssavepath + "/ --force-local" + " > "+ sc4rbpath+ "/.c4rb/log").c_str() );
+
+
+		return 1;
 
 	}else if(s == 2 || s == 3){
 		//打开save.list文件
@@ -75,10 +88,15 @@ int sys(int s){
 		//将新的到的列表打包
 
 
-	else{
+	}else{
 
 	}
 
+	return 1;
 
+}
+
+
+int AddSaveList(ofstream fout_in,string spath){
 
 }
