c9675680ea88edea1e2b61f7e4cb0a8be9c79a31
bsplu <ruc.bs.plu@hotmail.com>
2014-07-19 16:34:09 +0800

    建立分支，将char换为string

--- c4r.cpp
+++ c4r.cpp
@@ -15,12 +15,14 @@
 using namespace std;
 
 int sys(int s);
+void traval(char name[]);
 
 int main(){
 
 
-	sys(1);
-
+	//sys(1);
+	char name[250]=".\\log";
+	traval(name);
 
 	return 0;
 }
@@ -97,6 +99,125 @@ int sys(int s){
 }
 
 
-int AddSaveList(ofstream fout_in,string spath){
+int AddSaveList(ofstream fout_in,char spath[]){
 
+}
+
+void traval(char name[])//遍历一个目录
+		{
+	char name1[250];
+	//cout<<name;
+	//fprintf(fp,"%s  ****%d",name,++i);
+	_finddata_t file;
+	long lf;
+	if ((lf = _findfirst(name, &file)) == -1l) {
+		cout << "该文件或目录不存在！\n";
+		//fprintf(fp,"该目录不存在！\n");
+	} else {
+		//输入启示为文件
+
+		cout << file.name << "\t";
+		//fprintf(fp,"%s\t",file.name);
+		if (file.attrib == 0) {
+			cout << "正常 ";
+			//fprintf(fp,"正常 ");
+		}
+		if ((file.attrib % 2) == 1) {
+			cout << "只读 ";
+			//fprintf(fp,"只读 ");
+		}
+		if ((file.attrib == 2) || (file.attrib == 3) || (file.attrib == 6)
+				|| (file.attrib == 7) || (file.attrib == 18)
+				|| (file.attrib == 19) || (file.attrib == 22)
+				|| (file.attrib == 23) || (file.attrib == 34)
+				|| (file.attrib == 35) || (file.attrib == 38)
+				|| (file.attrib == 39) || (file.attrib == 50)
+				|| (file.attrib == 51) || (file.attrib == 54)
+				|| (file.attrib == 55)) {
+			//fprintf(fp,"%s\t",file.name);
+			cout << "隐藏 ";
+			//fprintf(fp,"隐藏 ");
+		}
+		if ((file.attrib == 4) || (file.attrib == 5) || (file.attrib == 6)
+				|| (file.attrib == 7) || (file.attrib == 20)
+				|| (file.attrib == 21) || (file.attrib == 22)
+				|| (file.attrib == 23) || (file.attrib == 36)
+				|| (file.attrib == 37) || (file.attrib == 38)
+				|| (file.attrib == 39) || (file.attrib == 52)
+				|| (file.attrib == 53) || (file.attrib == 54)
+				|| (file.attrib == 55)) {
+			cout << "系统 ";
+			//fprintf(fp,"系统 ");
+		}
+		if ((file.attrib >= 32 && file.attrib <= 55)) {
+			cout << "存档 ";
+			//fprintf(fp,"存档 ");
+		}
+
+		//输入启示为文件夹
+		if ((file.attrib >= 16 && file.attrib <= 23)
+				|| (file.attrib >= 48 && file.attrib <= 55)) {
+
+
+			while (_findnext(lf, &file) == 0) {
+				if (file.name[2] == 0)
+					continue;
+				cout << file.name << "\t";
+				//fprintf(fp,"%s\t",file.name);
+				if (file.attrib == 0) {
+					cout << "正常 ";
+					//fprintf(fp,"正常 ");
+				}
+				if ((file.attrib % 2) == 1) {
+					cout << "只读 ";
+					//fprintf(fp,"只读 ");
+				}
+				if ((file.attrib == 2) || (file.attrib == 3)
+						|| (file.attrib == 6) || (file.attrib == 7)
+						|| (file.attrib == 18) || (file.attrib == 19)
+						|| (file.attrib == 22) || (file.attrib == 23)
+						|| (file.attrib == 34) || (file.attrib == 35)
+						|| (file.attrib == 38) || (file.attrib == 39)
+						|| (file.attrib == 50) || (file.attrib == 51)
+						|| (file.attrib == 54) || (file.attrib == 55)) {
+					//fprintf(fp,"%s\t",file.name);
+					cout << "隐藏 ";
+					//fprintf(fp,"隐藏 ");
+				}
+				if ((file.attrib == 4) || (file.attrib == 5)
+						|| (file.attrib == 6) || (file.attrib == 7)
+						|| (file.attrib == 20) || (file.attrib == 21)
+						|| (file.attrib == 22) || (file.attrib == 23)
+						|| (file.attrib == 36) || (file.attrib == 37)
+						|| (file.attrib == 38) || (file.attrib == 39)
+						|| (file.attrib == 52) || (file.attrib == 53)
+						|| (file.attrib == 54) || (file.attrib == 55)) {
+					cout << "系统 ";
+					//fprintf(fp,"系统 ");
+				}
+				if ((file.attrib >= 32 && file.attrib <= 55)) {
+					cout << "存档 ";
+					//fprintf(fp,"存档 ");
+				}
+				if ((file.attrib >= 16 && file.attrib <= 23)
+						|| (file.attrib >= 48 && file.attrib <= 55)) {
+					cout << "文件夹 ";
+					cout << endl;
+					//fprintf(fp,"文件夹\n");
+					strcpy(name1, name);
+					name[strlen(name) - 3] = '\0';  //最后三个字符去掉
+					name = strcat(name, file.name);  //
+					name = strcat(name, "\\*.*");
+					cout << endl << name << endl;
+					//fprintf(fp,"\n%s\n",name1);
+					traval(name);
+					strcpy(name, name1);
+				} else {
+					cout << endl;
+					//fprintf(fp,"\n");
+				}
+			}
+		}
+	}
+	_findclose(lf);
 }
