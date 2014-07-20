1aacb15897e51c805e861cb30fa529c99598dd91
bsplu <ruc.bs.plu@hotmail.com>
2014-07-20 12:25:01 +0800

    已经完成char2string，并且将输出改为需要的格式，但是如果输入为单个文件还没有改完

--- c4r.cpp
+++ c4r.cpp
@@ -11,35 +11,30 @@
 #include<time.h>
 #include <sstream>
 
-
 using namespace std;
 
 int sys(int s);
 void traval(string name_in);
 
-int main(){
-
+int main() {
 
 	//sys(1);
-	string name=".\\*.*";
+	string name = ".\\*.*";
 	traval(name);
 
 	return 0;
 }
-
 
 /*
  * 该函数主要作用是同步的时候，选择要同步的文件
  * in:s表示同步指令
  * out:函数是否执行成功
  */
-int sys(int s){
+int sys(int s) {
 
+	string ssavepath; //要同步的目录名字
 
-
-	string ssavepath;//要同步的目录名字
-
-	string sc4rbpath;//c4r储存的目录
+	string sc4rbpath; //c4r储存的目录
 	string starfilename;
 	stringstream stime;
 
@@ -47,17 +42,16 @@ int sys(int s){
 
 	sc4rbpath = "D:/workspace";
 
-
 	stime << int(time(NULL));
 	starfilename = (stime.str() + ".tar.gz").c_str();
 
 	//检查sc4r目录是否存在
-	if(_access((sc4rbpath+"/.c4rb").c_str(),0) == -1){
+	if (_access((sc4rbpath + "/.c4rb").c_str(), 0) == -1) {
 		system(("powershell \"mkdir " + sc4rbpath + "/.c4rb \"").c_str());
 	}
 
 	//如果要同步所有文件，则不会罗列出还没有同步的文件列表
-	if(s == 1){
+	if (s == 1) {
 		//表示全部同步
 
 		ofstream fout((sc4rbpath + "/.c4rb/save.list").c_str());
@@ -67,30 +61,30 @@ int sys(int s){
 		//AddSaveList(fout,ssavepath);
 
 		//将整个目录打包
-		system(("tar --exclude " + sc4rbpath + "/.c4rb -zcvf "+sc4rbpath+"/.c4rb/" + starfilename + " " +
-				ssavepath + "/ --force-local" + " > "+ sc4rbpath+ "/.c4rb/log").c_str() );
-
+		system(
+				("tar --exclude " + sc4rbpath + "/.c4rb -zcvf " + sc4rbpath
+						+ "/.c4rb/" + starfilename + " " + ssavepath
+						+ "/ --force-local" + " > " + sc4rbpath + "/.c4rb/log").c_str());
 
 		return 1;
 
-	}else if(s == 2 || s == 3){
+	} else if (s == 2 || s == 3) {
 		//打开save.list文件
 
 		//2表示新产生的文件全部同步 3表示需要用户选择需要增添或者要同步的文件
 		//增添列表
-		if(s == 2){
+		if (s == 2) {
 			//获得新文件单子，将单子直接
 
-		}else if( s == 3){
+		} else if (s == 3) {
 
-		}else{
+		} else {
 
 		}
 
 		//将新的到的列表打包
 
-
-	}else{
+	} else {
 
 	}
 
@@ -98,71 +92,47 @@ int sys(int s){
 
 }
 
-
-int AddSaveList(ofstream fout_in,char spath[]){
+int AddSaveList(ofstream fout_in, char spath[]) {
 
 }
 
-void traval(string name)//遍历一个目录
-		{
+//遍历一个目录
+void traval(string name) {
 
 	string name1;
-	//cout<<name;
-	//fprintf(fp,"%s  ****%d",name,++i);
 	_finddata_t file;
 	long lf;
 	if ((lf = _findfirst(name.c_str(), &file)) == -1l) {
 		cout << "该文件或目录不存在！\n";
-		//fprintf(fp,"该目录不存在！\n");
+		return;
 	} else {
 
-			while (_findnext(lf, &file) == 0) {
-				if (strcmp(file.name,"..") == 0)
-					continue;
-				cout << file.name << "\t";
-				//fprintf(fp,"%s\t",file.name);
-				if (file.attrib == 0) {
-					cout << "正常 ";
-					//fprintf(fp,"正常 ");
-				}
-				if ((file.attrib & _A_RDONLY) != 0) {
-					cout << "只读 ";
-					//fprintf(fp,"只读 ");
-				}
-				if ((file.attrib & _A_HIDDEN) != 0) {
-					//fprintf(fp,"%s\t",file.name);
-					cout << "隐藏 ";
-					//fprintf(fp,"隐藏 ");
-				}
-				if ((file.attrib & _A_SYSTEM) != 0) {
-					cout << "系统 ";
-					//fprintf(fp,"系统 ");
-				}
-				if ((file.attrib & _A_ARCH) != 0) {
-					cout << "存档 ";
-					//fprintf(fp,"存档 ");
-				}
+		while (_findnext(lf, &file) == 0) {
+			if (strcmp(file.name, "..") == 0)
+				continue;
 
-				if ((file.attrib & _A_SUBDIR) != 0) {
-					cout << "文件夹 ";
-					cout << endl;
-					//fprintf(fp,"文件夹\n");
-					name1 = name;
+			if (file.attrib == 0) {
+				name1 = name;
+				cout << (name1.erase(name.length() - 3, 3)) + file.name << endl;
 
-					name.insert(name.length()-3,strcat(file.name,"\\"));
+			}
+			if ((file.attrib & _A_ARCH) != 0) {
+				name1 = name;
+				cout << (name1.erase(name.length() - 3, 3)) + file.name << endl;
 
-					cout << name << endl;
-
-					//traval(name);
-					cout<<endl;
-
-					name = name1;
-				} else {
-					cout << endl;
-					//fprintf(fp,"\n");
-				}
 			}
 
+			if ((file.attrib & _A_SUBDIR) != 0) {
+
+				name1 = name;
+
+				name.insert(name.length() - 3, strcat(file.name, "\\"));
+
+				traval(name);
+
+				name = name1;
+			}
+		}
 
 	}
 	_findclose(lf);
